69b037b0a1d081d0a10864c25a590a96726ef15d
Somuah Emmanuel <emmsom506@gmail.com>
2023-08-29 16:12:55 +0000

    Updated homepage backend codes

--- src/application/dental/homepage/HomePageController.java
+++ src/application/dental/homepage/HomePageController.java
@@ -1,5 +1,54 @@
 package application.dental.homepage;
 
-public class HomePageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import lib.DButil;
+
+public class HomePageController implements Initializable{
+	/*
+	 * fxml injection of the nodes id's
+	 */
+	@FXML
+	private Button homeBtn , aboutBtn,contactBtn,accountBtn;
+
+	@FXML
+	private Button seeMoreBtn,bookNowBtn,contactUsBtn;
+	;
+	@FXML
+	private Button logoutBtn;
+
+	@FXML
+	private Label welcomeLabel;
+
+
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resource) {
+		logoutBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				DButil.changeScene(event,"login.fxml", "Log in!", null);
+
+			}
+
+		});
+
+	}
+
+	/*
+	 * method to set welcome message and a username
+	 */
+	public void setUserInformation(String username) {
+		welcomeLabel.setText("Welcome," + username + "!");
+
+	}
 
 }
