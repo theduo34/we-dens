b167d81df8f34923b17851468701ccbfd0ffb396
Somuah Emmanuel <emmsom506@gmail.com>
2023-08-29 16:08:14 +0000

    Updated main file

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,8 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -10,20 +11,22 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application  {
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root =FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			primaryStage.setTitle("Log in!");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
