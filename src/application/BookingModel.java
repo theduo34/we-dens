25fcaf2685d30ef7a70f29c4a4a377359c204018
Somuah Emmanuel <emmsom506@gmail.com>
2023-09-11 11:11:57 +0000

    Deleted the setters

--- src/application/BookingModel.java
+++ src/application/BookingModel.java
@@ -2,13 +2,13 @@ package application;
 
 public class BookingModel {
 
-	Integer id;
-	String firstName;
-	String lastName;
-	String phone;
-	String location;
-	String email;
-	String date;
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String location;
+	private String email;
+	private String date;
 
 	public BookingModel(Integer id,String firstName, String lastName, String phone, String location,String email, String date) {
 		this.id = id;
@@ -26,43 +26,23 @@ public class BookingModel {
 	}
 
 	public String getFirstName() {
-		return  firstName;
+		return firstName;
 	}
 	public String getLastName() {
-		return  lastName;
+		return lastName;
 	}
 	public String getPhone() {
-		return  phone;
+		return phone;
 	}
 	public String getLocation() {
-		return  location;
+		return location;
 	}
 	public String getEmail() {
-		return  email;
+		return email;
 	}
 	public String getDate() {
-		return  date;
+		return date;
 	}
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
+	
 }
