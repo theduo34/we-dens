b7ca0500b957fd861613586640e71fcc4622cc8e
Somuah Emmanuel <emmsom506@gmail.com>
2023-09-11 11:21:12 +0000

    Updated the .class files

--- src/lib/DButil.java
+++ src/lib/DButil.java
@@ -21,39 +21,32 @@ import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 /*
- * utility class is act as the main of operation
- * to do all the communication ,to send and update user,
- * and changing scene to the correct one as in coperation with
- * the database connection
+ * class set to be the main of operation
+ * to do all the communication ,request and update,
+ * in coperation with the database connection
  * 
  */
-
-
 public class DButil {
-
-
-	private static String dbUrl="jdbc:mysql://localhost:3306/wedens";   //database info
+    private static String dbUrl="jdbc:mysql://localhost:3306/wedens";   //database info
 	private static String dbUsername="root";
 	private static String dbPassword="";
 
 	public Connection connect;
 
 	public Connection getConnection() {
-
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connect = DriverManager.getConnection(dbUrl,dbUsername,dbPassword );
-
 		}catch(Exception e) {
 			e.getCause();
 			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setTitle("database connection error");
-			alert.setHeaderText(null);
+			alert.setTitle("Connection Error!");
+			alert.setHeaderText("Error!");
 			alert.setContentText("Unable to make a connection to database, please check your connection!!!");
 			alert.showAndWait();
 		}
+
 		return connect;
+		
 	}
-
-
 }
